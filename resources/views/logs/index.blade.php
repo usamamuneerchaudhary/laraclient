94528d97fe185da20df8476cfa8be76b2f0cf1a0
Usama Muneer <usama@rentable.pk>
2023-03-26 13:08:56 +0100

    pagination for logs

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lara Client Logs</title>
     <link href="https://cdn.jsdelivr.net/npm/prismjs@1.28.0/themes/prism.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/prismjs@1.28.0/prism.js"></script>
     <style>
         body {
@@ -56,6 +57,7 @@
         </div>
         <hr>
     @endforeach
+    {{$logs->links('pagination::bootstrap-4')}}
 @else
     <h2>No logs found</h2>
 @endif
