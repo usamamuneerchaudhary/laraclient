07a7b3e480f3c969259556269b17ddcb198255f2
Usama Munir <hello@usamamuneer.me>
2026-07-25 21:53:08 +0500

    updated routes and added single log view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 use Usamamuneerchaudhary\LaraClient\Http\Controllers\LogsController;
 
-Route::get('/logs', [LogsController::class, 'index'])->name('logs.index');
+Route::get('/logs', [LogsController::class, 'index'])->name('laraclient.logs.index');
+Route::get('/logs/{log}', [LogsController::class, 'show'])->name('laraclient.logs.show');
